2071b9c7ca7831f9b71e3360493c9ccc7b5fdfa6
Krzysztof Antoszek <k.antoszek@gadu-gadu.pl>
2012-09-19 07:53:16 +0200

    abandoned

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ wrcbash
 =======
 
 A simple top-perspective WRC game
+
+This project abandoned. If you want to use it, rewrite it, do as you
+please. I would love to hear about the work you made, or changes.
+
+Please bare in mind, that this software is distributed "as is" without
+any guarantees. I'm not responsible for any damage done with it or the
+way anybody uses it.
+
+That said, have fun.
---
+++ src/wrcb/core/utils.js
@@ -0,0 +1,106 @@
+mamd.define("wrcb.core.utils", function () {
+    var requestFrame = (function () {
+        return window.requestAnimationFrame
+            || window.webkitRequestAnimationFrame
+            || window.mozRequestAnimationFrame
+            || window.oRequestAnimationFrame
+            || window.msRequestAnimationFrame
+            || function (callback){
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
+
+    return {
+
+        "decodeJson": function (data) {
+            try {
+                return JSON.parse(data);
+            } catch (e) {
+                !!DEBUG && console.error(e);
+            }
+            return null;
+        },
+
+        "filter": function (arr, func, boundTo) {
+            var bound = boundTo || arr,
+                l = arr.length,
+                i = 0,
+                result = [];
+            if ("filter" in arr) {
+                return arr.filter.call(bound, func);
+            }
+
+            for (; i < l; i++) {
+                if (!!func.call(boundTo, arr[i], i, arr)) {
+                    result.push(arr[i]);
+                }
+            }
+
+            return result;
+        },
+
+        "getRequestFrameFunction": function () {
+            return requestFrame;
+        },
+
+        "bind": function (func, oThis) {
+
+            if (typeof func !== "function") {
+                // closest thing possible to the ECMAScript 5 internal IsCallable function
+                throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+            }
+
+            var args = Array.prototype.slice.call(arguments, 2),
+                fToBind = func,
+                fNOP = function () {},
+                fBound = function () {
+                    return fToBind.apply(func instanceof fNOP
+                        ? this
+                        : oThis,
+                        args.concat(Array.prototype.slice.call(arguments)));
+                };
+
+            if ("bind" in func) {
+                return func.bind.apply(func, Array.prototype.slice.call(arguments, 1));
+            }
+
+            fNOP.prototype = func.prototype;
+            fBound.prototype = new fNOP();
+
+            return fBound;
+        },
+
+        "indexOf": function (haystack, needle) {
+            var l = haystack.length;
+            if (haystack.indexOf) {
+                return haystack.indexOf.call(haystack, needle);
+            }
+
+            while (--l >= 0) {
+                if (haystack[l] === needle) {
+                    return l;
+                }
+            }
+
+            return -1;
+        },
+
+        "bindEvent": function (element) {
+            var bindFunc = element.attachEvent || element.addEventListener,
+                args = Array.prototype.slice.call(arguments, 1);
+            bindFunc.apply(element, args);
+        },
+
+        "unbindEvent": function (element) {
+            var bindFunc = element.dettachEvent || element.removeEventListener,
+                args = Array.prototype.slice.call(arguments, 1);
+            bindFunc.apply(element, args);
+        },
+
+        "getColor": function (r, g, b, a) {
+            return !!a
+                ? ['rgba(', [r, g, b, a].join(','), ')'].join('')
+                : ['rgb(', [r, g, b].join(','), ')'].join('');
+        }
+    }
+});
